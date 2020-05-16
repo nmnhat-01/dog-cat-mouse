9a1388730dfa4c4fff4fa6ec3da47356c310b0e2
Nguyễn Minh Nhật <nhatnguyen0925230317@gmail.com>
2020-05-16 16:40:11 +0700

    add method eat for dog

--- Dog.js
+++ Dog.js
@@ -2,4 +2,8 @@ function dog() {
     this.weight = '3kg';
     this.height = "0.5m";
     this.age = '2';
+    this.stomach = [];
+}
+dog.prototype.eat = function(food){
+    this.stomach.push(food);
 }
